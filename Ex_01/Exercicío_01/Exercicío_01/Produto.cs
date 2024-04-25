9c9bc7754c60df4e322f8977c75448a1b9cf37eb
GuiNakamuraC <124791730+GuiNakamuraC@users.noreply.github.com>
2024-04-25 12:27:22 -0300

    Add files via upload

---
+++ Ex_01/Exercicío_01/Exercicío_01/Produto.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicío_01
+{
+    public class Produto
+    {
+        public string Nome { get; set; }
+        public double Preco { get; set; }
+        public int QuantidadeEmEstoque { get; private set; }
+
+        public Produto(string nome, double preco, int quantidadeEmEstoque)
+        {
+            Nome = nome;
+            Preco = preco;
+            QuantidadeEmEstoque = quantidadeEmEstoque;
+        }
+
+        public void AdicionarAoEstoque(int quantidade)
+        {
+            QuantidadeEmEstoque += quantidade;
+            Console.WriteLine($"{quantidade} unidades de {Nome} adicionadas ao estoque.");
+        }
+
+        public void RemoverDoEstoque(int quantidade)
+        {
+            if (quantidade > QuantidadeEmEstoque)
+            {
+                Console.WriteLine($"Não há {quantidade} unidades de {Nome} disponíveis no estoque.");
+            }
+            else
+            {
+                QuantidadeEmEstoque -= quantidade;
+                Console.WriteLine($"{quantidade} unidades de {Nome} removidas do estoque.");
+            }
+        }
+    }
+}
