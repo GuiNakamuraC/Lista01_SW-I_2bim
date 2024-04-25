34c57594af03a31d76f7a9d5e81224c943a377d4
GuiNakamuraC <124791730+GuiNakamuraC@users.noreply.github.com>
2024-04-25 12:30:13 -0300

    Add files via upload

---
+++ Exemplo/Exemplo_Construtor/Exemplo_Construtor/Cliente.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exemplo_Construtor
+{
+    internal class Cliente
+    {
+        public string? Nome { get; set; }
+        public string? Email { get; set; }
+        public int Senha { get; set; }
+
+        public Cliente(string nomeConstrutor, int senhaConstrutor)
+            {
+                Nome = nomeConstrutor;
+                Senha = senhaConstrutor;
+            }
+
+        public void MostraDados()
+        {
+            Console.WriteLine("Nome: " + Nome);
+            Console.WriteLine("E-mail: " + Email);
+            Console.WriteLine("Senha: " + Senha);
+        }
+    }
+}
---
+++ Exemplo/Exemplo_Construtor/Exemplo_Construtor/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Exemplo_Construtor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("EXEMPLO CONSTRUTOR DE UMA CLASSE");
+
+            Cliente cliente = new Cliente();
+
+            //cliente.Nome = "GUILHERME";
+            //cliente.Email = "guincarvalho08@gmail.com";
+            //cliente.Senha = 1234;
+
+            cliente.MostraDados();
+        }
+    }
+}
