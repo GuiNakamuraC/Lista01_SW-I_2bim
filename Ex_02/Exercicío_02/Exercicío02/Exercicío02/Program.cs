0e12f544002b2c31bb66541829d4b912f6744bec
GuiNakamuraC <124791730+GuiNakamuraC@users.noreply.github.com>
2024-05-16 12:22:06 -0300

    Add files via upload

---
+++ Ex_02/Exercicío_02/Exercicío02/Exercicío02/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Exercicio02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            RegistroDeCompras registroDeCompras = new RegistroDeCompras();
+
+            registroDeCompras.AdicionarCompra(new DateTime(2025, 5, 1), "Arroz", 50.0);
+            registroDeCompras.AdicionarCompra(new DateTime(2024, 10, 20), "Leite", 4.99);
+            registroDeCompras.AdicionarCompra(new DateTime(2025, 5, 5), "Caviar", 99.99);
+
+            registroDeCompras.ListarCompras();
+        }
+    }
+}
