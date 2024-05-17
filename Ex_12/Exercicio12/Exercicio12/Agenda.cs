3e170fd165b811d56d060da1d1927c3b28091804
GuiNakamuraC <124791730+GuiNakamuraC@users.noreply.github.com>
2024-05-17 18:38:10 -0300

    Add files via upload

---
+++ Ex_12/Exercicio12/Exercicio12/Agenda.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Exercicio12
+{
+    public class Agenda
+    {
+        public class Pessoa
+        {
+            public string Nome { get; set; }
+            public int Idade { get; set; }
+            public float Altura { get; set; }
+
+            // Construtor da classe Pessoa
+            public Pessoa(string nome, int idade, float altura)
+            {
+                Nome = nome;
+                Idade = idade;
+                Altura = altura;
+            }
+
+            // Sobrescrevendo o método ToString para formatar a exibição dos dados da pessoa
+            public override string ToString()
+            {
+                return $"Nome: {Nome}, Idade: {Idade}, Altura: {Altura}";
+            }
+        }
+
+        // Lista para armazenar as pessoas na agenda
+        private List<Pessoa> pessoas = new List<Pessoa>();
+
+        // Método para armazenar uma pessoa na agenda
+        public void ArmazenaPessoa(string nome, int idade, float altura)
+        {
+            pessoas.Add(new Pessoa(nome, idade, altura));
+        }
+
+        // Método para remover uma pessoa da agenda pelo nome
+        public void RemovePessoa(string nome)
+        {
+            Pessoa pessoa = BuscaPessoa(nome);
+            if (pessoa != null)
+                pessoas.Remove(pessoa);
+        }
+
+        // Método para buscar uma pessoa na agenda pelo nome
+        public Pessoa BuscaPessoa(string nome)
+        {
+            return pessoas.Find(p => p.Nome == nome);
+        }
+
+        // Método para imprimir os dados de todas as pessoas da agenda
+        public void ImprimeAgenda()
+        {
+            foreach (var pessoa in pessoas)
+            {
+                Console.WriteLine(pessoa);
+            }
+        }
+    }
+}
---
+++ Ex_12/Exercicio12/Exercicio12/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace Exercicio12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instanciando a agenda
+            Agenda agenda = new Agenda();
+            string opcao;
+
+            // Loop principal do menu
+            do
+            {
+                Console.WriteLine("\nMenu:");
+                Console.WriteLine("1 - Armazenar Pessoa");
+                Console.WriteLine("2 - Remover Pessoa");
+                Console.WriteLine("3 - Buscar Pessoa");
+                Console.WriteLine("4 - Imprimir Agenda");
+                Console.WriteLine("0 - Sair");
+                Console.Write("Escolha uma opção: ");
+                opcao = Console.ReadLine();
+
+                switch (opcao)
+                {
+                    case "1":
+                        // Armazena
+                        Console.Write("Nome: ");
+                        string nome = Console.ReadLine();
+                        Console.Write("Idade: ");
+                        int idade = int.Parse(Console.ReadLine());
+                        Console.Write("Altura: ");
+                        float altura = float.Parse(Console.ReadLine());
+                        agenda.ArmazenaPessoa(nome, idade, altura);
+                        break;
+                    case "2":
+                        // Remove
+                        Console.Write("Nome: ");
+                        string nomeRemover = Console.ReadLine();
+                        agenda.RemovePessoa(nomeRemover);
+                        break;
+                    case "3":
+                        // Busca
+                        Console.Write("Nome: ");
+                        string nomeBuscar = Console.ReadLine();
+                        Agenda.Pessoa pessoaEncontrada = agenda.BuscaPessoa(nomeBuscar);
+                        if (pessoaEncontrada != null)
+                            Console.WriteLine($"Pessoa encontrada: {pessoaEncontrada}");
+                        else
+                            Console.WriteLine("Pessoa não encontrada.");
+                        break;
+                    case "4":
+                        // Imprime
+                        Console.WriteLine("Agenda:");
+                        agenda.ImprimeAgenda();
+                        break;
+                    case "0":
+                        // Sai do programa
+                        Console.WriteLine("Saindo...");
+                        break;
+                    default:
+                        // Opção inválida
+                        Console.WriteLine("Opção inválida.");
+                        break;
+                }
+
+            } while (opcao != "0"); // Continuar o loop enquanto a opção for diferente de 0
+        }
+    }
+}
